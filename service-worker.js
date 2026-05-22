dd6d72ca3a74875deeb44c6e73543e2f467cc791
yoshida.t <yoshida.t@vstone.co.jp>
2026-05-22 14:45:50 +0900

    Place mobile mapping labels beside controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'web-bluetooth-vsc4-20260522-5';
+const CACHE_NAME = 'web-bluetooth-vsc4-20260522-6';
 
 function addToCache(request, networkResponse) {
   if (request.method !== 'GET' || !networkResponse || !networkResponse.ok) {
