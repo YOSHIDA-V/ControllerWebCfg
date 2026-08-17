70fbc17084afabdbfdc1dff92e022c8297ac49dc
yoshida.t <yoshida.t@vstone.co.jp>
2026-08-17 16:51:21 +0900

    Redesign VS-C4 web interface

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'web-bluetooth-vsc4-20260817-5';
+const CACHE_NAME = 'web-bluetooth-vsc4-20260817-6';
 
 function addToCache(request, networkResponse) {
   if (request.method !== 'GET' || !networkResponse || !networkResponse.ok) {
